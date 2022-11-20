36c7b3ab9c2c72a9b1f2acbc96a623e5f26f4b48
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2022-11-20 13:55:59 -0700

    Update Home.jsx
    updated the Home component to use the custom menu component.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState } from 'react'
+import { Link, Navigate } from 'react-router-dom'
+import Menu from './Menu'
 
-const Home = ({}) => {
-    return (
-        <div id='menu'>
-            <div id='menu-items'>
-                <p>THIS IS HOME</p>
-                <Link to='/newline'>NewLine</Link>
-                <Link to='/playline'>PlayLine</Link>
-                <Link to='/editline'>Edit Line</Link>
-                <Link to='/endless'>Endless Play</Link>
-            </div>
-        </div>
-    )
+const Home = () => {
+    const [option, setOption] = useState(false)
+    if (!option){
+        return (
+            <Menu items={['New Line', 'Edit Line', 'Play Line', 'Endless', 'Help']} menuName={'Main Menu'} setOption={setOption}/>
+        )
+    } else {
+        switch(option){
+            case 'New Line':
+                return <Navigate to='/newline'/>
+                break
+            case 'Edit Line':
+                return <Navigate to='/editline'/>
+                break
+            case 'Play Line':
+                return <Navigate to='/playline'/>
+                break
+            case 'Endless':
+                return <Navigate to='/endless'/>
+                break
+            default:
+                return <Navigate to='/'/>
+        }
+    }
 }
 
 export default Home
