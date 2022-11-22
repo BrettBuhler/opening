33e152a0578e611877a8690b38e077aa171a8cd1
BrettBuhler <107363334+BrettBuhler@users.noreply.github.com>
2022-11-22 06:35:50 -0700

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,14 +1,15 @@
 import MenuItem from "./MenuItem"
+import { Box } from "@mui/system"
 
 const Menu = ({ items, menuName, setOption}) => {
     const onClick = (event) => {
         setOption(event.target.value)
     }
     return (
-        <div>
+        <Box>
             <h2>{menuName}</h2>
             {items.map((x,i)=><MenuItem item={x} key={i} onClick={onClick}/>)}
-        </div>
+        </Box>
     )
 }
 
